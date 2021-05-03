aa673dd93e59e87ecb34551a6d6ca2545c697202
floodbaker <83601610+floodbaker@users.noreply.github.com>
2021-05-03 19:44:27 +0300

    Update not-hesap.py

--- not-hesap.py
+++ not-hesap.py
@@ -1,27 +1,3 @@
-"""Kullanıcının girdiği vize1, vize2, final notlarına notlarına göre harf notunu hesaplayın.
-vize1 toplam notun %30'una etki edecek.
-vize2 toplam notun %30'una etki edecek.
-final toplam notun %40'ına etki edecek. (Bu değerler 0-100 arası kabul edilmeli.)
-
-Toplam Not >=  90 -----> AA
-
-    Toplam Not >=  85 -----> BA
-
-    Toplam Not >=  80 -----> BB
-
-    Toplam Not >=  75 -----> CB
-
-    Toplam Not >=  70 -----> CC
-
-    Toplam Not >=  65 -----> DC
-
-    Toplam Not >=  60 -----> DD
-
-    Toplam Not >=  55 -----> FD
-
-     Toplam Not <  55 -----> FF
-"""
-
 while True:
     vize1 = int(input("Vize-1 değerini giriniz: "))
     if vize1 > 100 or vize1 < 0:
